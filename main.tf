0852ce08e85a1d0017ad07eb2e550165c309c5da
Sho-ib <131812622+Sho-ib@users.noreply.github.com>
2023-05-01 13:53:04 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,3 +27,7 @@ resource "aws_s3_bucket" "example" {
   bucket = "shoaibterraformdemonew"
   acl    = "private"
 }
+resource "aws_s3_bucket" "example2" {
+  bucket = "NewdemoshoibS3"
+  acl    = "private"
+}
